47a00b470f268ba48829615f3e5fc1c2f6e639c4
Haroon Rashid <haroon.rashid235@gmail.com>
2016-08-22 16:40:10 +0500

    Record readings from gyroscope sensor and display them on Main Activity.

--- app/src/main/java/com/example/android/breathe/MainActivity.java
+++ app/src/main/java/com/example/android/breathe/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.android.breathe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    TextView gyroText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create instances of Sensor and SensorManager to work with sensors.Sensor is initialized with Gyroscope Sensor.
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        // Register a Listener to record sensor values whenever they change
+        mSensorManager.registerListener(this, mSensor , SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        gyroText = (TextView) findViewById(R.id.gyro_text);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // Update the UI with the results from gyroscope
+        gyroText.setText("X :" + sensorEvent.values[0] + "\n\nY : " + sensorEvent.values[1] + "\n\nZ : " + sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // no Changes needed.
     }
 }
