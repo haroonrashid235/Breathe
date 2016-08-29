4627206e2c77586e79c24c3982da38771d81d7fb
Haroon Rashid <haroon.rashid235@gmail.com>
2016-08-29 16:23:12 +0500

    Code as on 29/8/2016 4:22 PM

--- app/src/main/java/com/example/android/breathe/MainActivity.java
+++ app/src/main/java/com/example/android/breathe/MainActivity.java
@@ -8,6 +8,9 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,21 +18,26 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private static final int DATA_SIZE = 101;
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private TextView gyroText;
     private GraphView graph;
     LineGraphSeries<DataPoint> series;
-    private DataPoint[] data = new DataPoint[11];
+    private DataPoint[] data = new DataPoint[101];
     private int i = 0;
 
     // Change this value to increase or decrease the sensitivity (This is multiplying factor)
-    private static final int SENSITIVITY = 10;
+    private static final double SCALER = (180.0/3.14);
 
     // Sampling Time
-    private static final int SAMPLING_TIME = 50; //milliseconds
+    private static final int SAMPLING_TIME = 150; //milliseconds
 
 
 
@@ -39,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         for(int i=0;i<data.length; i++){
-            data[i] = new DataPoint(i,i);
+            data[i] = new DataPoint(0,0);
         }
 
         // Create instances of Sensor and SensorManager to work with sensors.Sensor is initialized with Gyroscope Sensor.
@@ -65,13 +73,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // set manual X bounds
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(10);
+        graph.getViewport().setMaxX(100);
 
 
         // set manual Y bounds
         graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(-6);
-        graph.getViewport().setMaxY(6);
+        graph.getViewport().setMinY(-20);
+        graph.getViewport().setMaxY(20);
 
 
         // Timer that Runs the Code after specified number of milliseconds
@@ -87,20 +95,65 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         };
         handler.post(run);
 
+        Button record_button = (Button) findViewById(R.id.record_data);
+        record_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
+
+
+
     @Override
     public void onSensorChanged(final SensorEvent sensorEvent) {
 
-        data[i] = new DataPoint(i,SENSITIVITY * sensorEvent.values[0]);
-        i++;
-        if(i > 10){
-            i = 0;
-        }
+        double scaled_value = SCALER * sensorEvent.values[0];
+        double roundOffX = Math.round(scaled_value * 100.0) / 100.0;
 
+        if( scaled_value < 1.5 && scaled_value > -1.5){
+            scaled_value = 0;
+        }
 
         // Update the UI with the results from gyroscope
         gyroText.setText("X :" + sensorEvent.values[0] + "\n\nY : " + sensorEvent.values[1] + "\n\nZ : " + sensorEvent.values[2]);
+        if (scaled_value < 20 && scaled_value > -20){
+            data[i] = new DataPoint(i, roundOffX);
+            i++;
+            Log.d("onSensorChanged",String.valueOf(scaled_value));
+            if(i > data.length - 1){
+                i = 0;
+                for(int j =0 ;j < data.length ; j++){
+                    data[j] = new DataPoint(0,0);
+                }
+            }
+        }
+
+
+
+            // Writing to a file
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("myFile.csv",false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            writer.append(String.valueOf(roundOffX));
+            writer.append(",");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
